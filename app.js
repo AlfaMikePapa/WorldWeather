ddacc9b142dd629c661c136e0e6f70c188893a46
Aleksi Putkonen <64532516+AlfaMikePapa@users.noreply.github.com>
2021-08-09 10:08:28 +0000

    Bug fixes in app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const currentTempEl = document.getElementById('current-temp');
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-const API_KEY ='f09c158a26f89df814d9fef2f3c256ea'
+const API_KEY ='f09c158a26f89df814d9fef2f3c256ea';
 
 setInterval(() => {
   const time = new Date();
@@ -18,7 +18,7 @@ setInterval(() => {
   const date = time.getDate();
   const day = time.getDay();
   const hour = time.getHours();
-  const hoursIn12HrFormat = hour >=13 ? hour %12: hour
+  const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
   const minutes = time.getMinutes();
   const ampm = hour >=12 ? 'PM' : 'AM'
 
@@ -108,5 +108,5 @@ data.daily.forEach((day, idx) => {
 })
 
 
-weatherForecastEl.innerHTML = otherDayForecast;
+weatherForecastEl.innerHTML = otherDayForcast;
 }
